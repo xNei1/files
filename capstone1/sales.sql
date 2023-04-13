9d093da2155abcb23736045b97db36b4099903e7
xNei1 <neil09557082751@gmail.com>
2023-04-13 14:43:12 +0800

    backup

--- capstone1/sales.sql
+++ capstone1/sales.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2018 at 08:51 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Apr 13, 2023 at 08:40 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `sales`
@@ -26,14 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
   `username` varchar(24) NOT NULL,
   `password` varchar(12) NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `middlename` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -49,7 +50,7 @@ INSERT INTO `admin` (`admin_id`, `username`, `password`, `firstname`, `middlenam
 -- Table structure for table `book`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
+CREATE TABLE `book` (
   `book_id` int(11) NOT NULL,
   `book_title` varchar(50) NOT NULL,
   `book_description` varchar(200) NOT NULL,
@@ -57,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `book` (
   `book_author` varchar(60) NOT NULL,
   `date_publish` varchar(25) NOT NULL,
   `qty` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `book`
@@ -73,14 +74,14 @@ INSERT INTO `book` (`book_id`, `book_title`, `book_description`, `book_category`
 -- Table structure for table `borrowing`
 --
 
-CREATE TABLE IF NOT EXISTS `borrowing` (
+CREATE TABLE `borrowing` (
   `borrow_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `student_no` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `date` date NOT NULL,
   `status` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `borrowing`
@@ -102,10 +103,10 @@ INSERT INTO `borrowing` (`borrow_id`, `book_id`, `student_no`, `qty`, `date`, `s
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `name` varchar(250) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `category`
@@ -113,7 +114,8 @@ CREATE TABLE IF NOT EXISTS `category` (
 
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'Drinks'),
-(2, 'Food');
+(2, 'Food'),
+(3, 'Medicines');
 
 -- --------------------------------------------------------
 
@@ -121,7 +123,7 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- Table structure for table `collection`
 --
 
-CREATE TABLE IF NOT EXISTS `collection` (
+CREATE TABLE `collection` (
   `transaction_id` int(11) NOT NULL,
   `date` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -137,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `collection` (
 -- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE `customer` (
   `customer_id` int(11) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
@@ -146,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `prod_name` varchar(550) NOT NULL,
   `expected_date` varchar(500) NOT NULL,
   `note` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
@@ -161,7 +163,7 @@ INSERT INTO `customer` (`customer_id`, `customer_name`, `address`, `contact`, `m
 -- Table structure for table `products`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
+CREATE TABLE `products` (
   `product_id` int(11) NOT NULL,
   `product_code` varchar(200) NOT NULL,
   `gen_name` varchar(200) NOT NULL,
@@ -176,14 +178,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `qty_sold` int(10) NOT NULL,
   `expiry_date` varchar(500) NOT NULL,
   `date_arrival` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`product_id`, `product_code`, `gen_name`, `product_name`, `cost`, `o_price`, `price`, `profit`, `supplier`, `onhand_qty`, `qty`, `qty_sold`, `expiry_date`, `date_arrival`) VALUES
-(1, 'RBH-f881', 'Drinks', 'Coke ', '', '65', '150', '85', 'Arogun Sunday', 0, 4, 100, '2018-02-07', '2018-02-06');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -191,7 +186,7 @@ INSERT INTO `products` (`product_id`, `product_code`, `gen_name`, `product_name`
 -- Table structure for table `purchases`
 --
 
-CREATE TABLE IF NOT EXISTS `purchases` (
+CREATE TABLE `purchases` (
   `transaction_id` int(11) NOT NULL,
   `invoice_number` varchar(100) NOT NULL,
   `date` varchar(100) NOT NULL,
@@ -205,7 +200,7 @@ CREATE TABLE IF NOT EXISTS `purchases` (
 -- Table structure for table `purchases_item`
 --
 
-CREATE TABLE IF NOT EXISTS `purchases_item` (
+CREATE TABLE `purchases_item` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `qty` int(11) NOT NULL,
@@ -219,7 +214,7 @@ CREATE TABLE IF NOT EXISTS `purchases_item` (
 -- Table structure for table `sales`
 --
 
-CREATE TABLE IF NOT EXISTS `sales` (
+CREATE TABLE `sales` (
   `transaction_id` int(11) NOT NULL,
   `invoice_number` varchar(100) NOT NULL,
   `cashier` varchar(100) NOT NULL,
@@ -230,7 +225,7 @@ CREATE TABLE IF NOT EXISTS `sales` (
   `due_date` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `balance` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sales`
@@ -239,7 +234,25 @@ CREATE TABLE IF NOT EXISTS `sales` (
 INSERT INTO `sales` (`transaction_id`, `invoice_number`, `cashier`, `date`, `type`, `amount`, `profit`, `due_date`, `name`, `balance`) VALUES
 (1, 'RS-0262328', 'Admin', '02/08/18', 'cash', '150', '85', '200', '', ''),
 (2, 'RS-233323', 'Admin', '02/08/18', 'cash', '13650', '7735', '14000', '', ''),
-(3, 'RS-0233302', 'Cashier Pharmacy', '02/12/18', 'cash', '150', '85', '100', '', '');
+(3, 'RS-0233302', 'Cashier Pharmacy', '02/12/18', 'cash', '150', '85', '100', '', ''),
+(4, 'RS-25428032', 'Administrator', '02/25/23', 'cash', '40', '10', '100', 'Neil', ''),
+(5, 'RS-0322303', 'Administrator', '02/25/23', 'cash', '20', '5', '30', 'joanna', ''),
+(6, 'RS-5333', 'Administrator', '02/25/23', 'cash', '', '', '112', '', ''),
+(7, 'RS-232063', 'Administrator', '02/25/23', 'cash', '', '', '1212122', '', ''),
+(8, 'RS-63005', 'Administrator', '02/25/23', 'cash', '20', '5', '30', 'joanna', ''),
+(9, 'RS-224933', 'Administrator', '02/25/23', 'cash', '', '', '12', 'jaonna', ''),
+(10, 'RS-224933', 'Administrator', '02/25/23', 'cash', '', '', '21', 'joanna', ''),
+(11, 'RS-303332', 'Administrator', '02/25/23', 'cash', '', '', '123', 'asdasd', ''),
+(12, 'RS-2033339', 'Administrator', '02/25/23', 'cash', '', '', '13', 'aw1', ''),
+(13, 'RS-36873', 'Administrator', '02/25/23', 'cash', '', '', '21', 'asdcasd', ''),
+(14, 'RS-232382', 'Administrator', '02/25/23', 'cash', '', '', '23', 'asdcas', ''),
+(15, 'RS-253320', 'Administrator', '02/25/23', 'cash', '500', '125', '200', 'joanna', ''),
+(16, 'RS-537923', 'Administrator', '02/25/23', 'cash', '20', '5', '25', 'joanna', ''),
+(17, 'RS-0490372', 'Administrator', '02/25/23', 'cash', '20', '5', '25', 'doroy', ''),
+(18, 'RS-3323302', 'Administrator', '02/25/23', 'cash', '', '', '25', 'doroy', ''),
+(19, 'RS-52223233', 'Administrator', '02/25/23', 'cash', '20', '5', '25', 'doroy', ''),
+(20, 'RS-5009033', 'Administrator', '02/25/23', 'cash', '', '', '25', 'asd', ''),
+(21, 'RS-332093', 'Administrator', '02/25/23', 'cash', '20', '5', '25', 'asd', '');
 
 -- --------------------------------------------------------
 
@@ -247,7 +260,7 @@ INSERT INTO `sales` (`transaction_id`, `invoice_number`, `cashier`, `date`, `typ
 -- Table structure for table `sales_order`
 --
 
-CREATE TABLE IF NOT EXISTS `sales_order` (
+CREATE TABLE `sales_order` (
   `transaction_id` int(11) NOT NULL,
   `invoice` varchar(100) NOT NULL,
   `product` varchar(100) NOT NULL,
@@ -260,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `sales_order` (
   `price` varchar(100) NOT NULL,
   `discount` varchar(100) NOT NULL,
   `date` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sales_order`
@@ -272,7 +285,17 @@ INSERT INTO `sales_order` (`transaction_id`, `invoice`, `product`, `qty`, `amoun
 (3, 'RS-0233302', '1', '1', '150', '85', 'RBH-f881', 'Drinks', 'Coke ', '150', '', '02/12/18'),
 (4, 'RS-86033', '1', '1', '150', '85', 'RBH-f881', 'Drinks', 'Coke ', '150', '', '02/13/18'),
 (5, 'RS-86033', '1', '1', '150', '85', 'RBH-f881', 'Drinks', 'Coke ', '150', '', '02/13/18'),
-(6, 'RS-23332024', '1', '1', '150', '85', 'RBH-f881', 'Drinks', 'Coke ', '150', '', '02/25/18');
+(6, 'RS-23332024', '1', '1', '150', '85', 'RBH-f881', 'Drinks', 'Coke ', '150', '', '02/25/18'),
+(7, 'RS-000030', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo ', '20', '', '02/25/23'),
+(8, 'RS-000030', '2', '2', '40', '10', 'RBH-f870', '', 'Coke mismo ', '20', '', '02/25/23'),
+(9, 'RS-25428032', '2', '2', '40', '10', 'RBH-f870', '', 'Coke mismo ', '20', '', '02/25/23'),
+(10, 'RS-0322303', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo ', '20', '', '02/25/23'),
+(11, 'RS-63005', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo ', '20', '', '02/25/23'),
+(12, 'RS-253320', '2', '25', '500', '125', 'RBH-f870', '', 'Coke mismo  ', '20', '', '02/25/23'),
+(13, 'RS-537923', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo  ', '20', '', '02/25/23'),
+(14, 'RS-0490372', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo  ', '20', '', '02/25/23'),
+(15, 'RS-52223233', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo  ', '20', '', '02/25/23'),
+(16, 'RS-332093', '2', '1', '20', '5', 'RBH-f870', '', 'Coke mismo  ', '20', '', '02/25/23');
 
 -- --------------------------------------------------------
 
@@ -280,7 +303,7 @@ INSERT INTO `sales_order` (`transaction_id`, `invoice`, `product`, `qty`, `amoun
 -- Table structure for table `student`
 --
 
-CREATE TABLE IF NOT EXISTS `student` (
+CREATE TABLE `student` (
   `student_id` int(11) NOT NULL,
   `student_no` varchar(8) NOT NULL,
   `firstname` varchar(30) NOT NULL,
@@ -288,7 +311,7 @@ CREATE TABLE IF NOT EXISTS `student` (
   `lastname` varchar(30) NOT NULL,
   `course` varchar(8) NOT NULL,
   `section` varchar(6) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
@@ -305,21 +328,23 @@ INSERT INTO `student` (`student_id`, `student_no`, `firstname`, `middlename`, `l
 -- Table structure for table `supliers`
 --
 
-CREATE TABLE IF NOT EXISTS `supliers` (
+CREATE TABLE `supliers` (
   `suplier_id` int(11) NOT NULL,
   `suplier_name` varchar(100) NOT NULL,
   `suplier_address` varchar(100) NOT NULL,
   `suplier_contact` varchar(100) NOT NULL,
   `contact_person` varchar(100) NOT NULL,
   `note` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `supliers`
 --
 
 INSERT INTO `supliers` (`suplier_id`, `suplier_name`, `suplier_address`, `suplier_contact`, `contact_person`, `note`) VALUES
-(1, 'Arogun Sunday', 'Ijebu Ode', 'Mr. Raphael ', '0902673484', 'They can lie for africa');
+(2, 'Medicines', 'Manila, Philippines', 'Neil David', '1234567890', 'Supplier 1'),
+(3, 'Drinks', 'Manila, Philippines', 'Neil David', '1234567890', 'Supplier 2'),
+(4, 'Food', 'Manila, Philippines', 'Neil David', '1234567890', 'Supplier 3');
 
 -- --------------------------------------------------------
 
@@ -327,22 +352,20 @@ INSERT INTO `supliers` (`suplier_id`, `suplier_name`, `suplier_address`, `suplie
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `position` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `name`, `position`) VALUES
-(2, 'tobi', 'tobi', 'Cashier Pharmacy', 'Cashier'),
-(3, 'admin', 'admin123', 'Administrator', 'admin'),
-(5, 'joy', 'joy', 'Alasagba', 'Cashier');
+(3, 'admin', 'admin123', 'Administrator', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -440,72 +463,87 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `borrowing`
 --
 ALTER TABLE `borrowing`
-  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `collection`
 --
 ALTER TABLE `collection`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `purchases`
 --
 ALTER TABLE `purchases`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `purchases_item`
 --
 ALTER TABLE `purchases_item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
 --
 -- AUTO_INCREMENT for table `sales_order`
 --
 ALTER TABLE `sales_order`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `supliers`
 --
 ALTER TABLE `supliers`
-  MODIFY `suplier_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `suplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
